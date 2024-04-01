a5921eafe9b132cb10a30b63a9c017ecc022f0e7
Chris Van Pelt <vanpelt@wandb.com>
2024-03-31 21:03:33 -0700

    Skip pre-commit config for now

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -13,7 +13,7 @@ pip install -e .[test]
 
 # addressing warning...
 git config --unset-all core.hooksPath
-pre-commit install
+pre-commit install --allow-missing-config
 
 # pre-commit hooks cause permission weirdness
 git config --global --add safe.directory /workspaces/openui
