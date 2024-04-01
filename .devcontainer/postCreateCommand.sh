a9fb7cbc57e3f013836ca73414b6012bec09ee4f
Chris Van Pelt <vanpelt@wandb.com>
2024-03-31 21:02:08 -0700

    Update frontend deps, first pass on a dev container

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fix cache perms
+mkdir -p $HOME/.cache
+sudo chown -R $USER $HOME/.cache
+
+# Ollama time
+curl -fsSL https://ollama.com/install.sh | sh
+
+# Install python packages
+cd /workspaces/openui/backend
+pip install -e .[test]
+
+# addressing warning...
+git config --unset-all core.hooksPath
+pre-commit install
+
+# pre-commit hooks cause permission weirdness
+git config --global --add safe.directory /workspaces/openui
