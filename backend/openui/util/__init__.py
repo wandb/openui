cb26f29afd0c63038bdcf9da4c339a1f07bfee23
Chris Van Pelt <vanpelt@wandb.com>
2024-05-15 15:15:56 +0100

    Dont pull in PIL and playwright unless we really need to

--- backend/openui/util/__init__.py
+++ backend/openui/util/__init__.py
@@ -1,4 +1,8 @@
-from .screenshots import gen_screenshots
+try:
+	from .screenshots import gen_screenshots
+except ImportError:
+	def gen_screenshots(*args, **kwargs):
+		pass
 
 __all__ = ["gen_screenshots"]
 
